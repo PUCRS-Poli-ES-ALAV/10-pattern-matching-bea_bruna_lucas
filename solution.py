f675ba60f40f25f86d275118d078460dab3793a3
Bia Cavalari <b.cavalari@edu.pucrs.br>
2025-05-26 20:24:22 -0300

    Counter..?

--- solution.py
+++ solution.py
@@ -1,6 +1,3 @@
-s1 = "ABCDCBDCBDACBDABDCBADF"
-s2 = "ADF"
-
 def func(s1,s2,i):
   if len(s2) == 0:
     return True
@@ -10,7 +7,48 @@ def func(s1,s2,i):
   except:
     return False
 
+s1 = "ABCDCBDCBDACBDABDCBADF"
+s2 = "ADF"
+
 for i in range(len(s1)):
   if func(s1,s2,i):
     print(i)
     break
+
+s1 = "AAAAAAAAAAAAB"
+s2 = "AAAB"
+
+for i in range(len(s1)):
+  if func(s1,s2,i):
+    print(i)
+    break
+
+def funcWCounter(s1, s2, i, count):
+    count[0] += 1
+    if len(s2) == 0:
+        return True
+    try:
+        if s1[i] == s2[0]:
+            return funcWCounter(s1, s2[1:], i+1, count)
+    except IndexError:
+        return False
+    return funcWCounter(s1, s2, i+1, count)
+
+s1 = "ABCDCBDCBDACBDABDCBADF"
+s2 = "ADF"
+
+count = [0]
+for i in range(len(s1)):
+    if funcWCounter(s1, s2, i, count):
+        print(f"Index: {i}, Iterations: {count[0]}")
+        break
+
+s1 = "AAAAAAAAAAAAB"
+s2 = "AAAB"
+
+count = [0]
+for i in range(len(s1)):
+    if funcWCounter(s1, s2, i, count):
+        print(f"Index: {i}, Iterations: {count[0]}")
+        break
+
